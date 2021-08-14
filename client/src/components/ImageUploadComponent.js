c15bb1db3e31336ab1e71790181c619c978db4c5
Ionut <ionut.dejeu@gamail.com>
2021-08-14 15:51:06 +0300

    Editor

--- client/src/components/ImageUploadComponent.js
+++ client/src/components/ImageUploadComponent.js
@@ -1,16 +1,14 @@
-import React,{useContext, useRef } from 'react';
-import {GlobalContext} from './StateProvider';
-import {store} from './AppEventStore'
+import React from 'react';
+ import {store} from './AppEventStore'
 
 function ImageUploadComponent(props) {
-    const loadedImageRef = useRef(null);
-    const { change_loaded_image } = useContext(GlobalContext);
+    
     const file_selection_changed = (ev)=>{
         var f = ev.target.files[0];
         var fr = new FileReader();
     
         fr.onload = (ev2)=>{
-            change_loaded_image(ev2.target.result);
+            
             store.emit('image_changed',ev2.target.result)
         };
         fr.readAsDataURL(f);
